c627140ada17f82020e46fe261e1faf0ac06f75b
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-10-09 22:24:18 -0300

    Finalização

---
+++ app.js
@@ -0,0 +1,9 @@
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 150 * i);
+    });
+}
+const titulo = document.querySelector('h1');
+typeWrite(titulo);
