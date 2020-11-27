d94a16041b93bdb158a13b758c9924a96e9e0694
carlossroliveira <63623377+carlossroliveira@users.noreply.github.com>
2020-11-27 10:58:49 -0300

    atualização

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function typeWrite(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 150 * i);
+        setTimeout(() => elemento.innerHTML += letra, 50* i);
     });
 }
 const titulo = document.querySelector('h1');
